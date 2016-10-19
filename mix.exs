131bddd8b2de62dea98b23edecb98480baf8d665
Andreas Böhrnsen <andreas@frontrunner.io>
2016-10-19 22:22:58 +0200

    add ‘start’ task

--- mix.exs
+++ mix.exs
@@ -55,8 +55,11 @@ defmodule Songbox.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "start": ["phoenix.server"],
+      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
   end
 end
