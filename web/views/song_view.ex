9c3f4f72ec807056bd90762b2dd5eebdf8a0e767
Andreas Böhrnsen <andreas@frontrunner.io>
2016-10-19 23:03:53 +0200

    fix ja_serializer deprecation warning

--- web/views/song_view.ex
+++ web/views/song_view.ex
@@ -3,6 +3,9 @@ defmodule Songbox.SongView do
   use JaSerializer.PhoenixView
 
   attributes [:title, :author, :key, :tempo, :time, :text, :format, :license, :ccli, :inserted_at, :updated_at]
-  
+
+  def format(song, _conn) do
+    song.format
+  end
 
 end
