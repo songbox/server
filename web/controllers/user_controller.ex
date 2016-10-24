e6a2d13f8d6da57713105450c4bf631da1cf4e53
Andreas Böhrnsen <andreas@frontrunner.io>
2016-10-24 16:59:11 +0200

    remove unused alias’

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,9 +1,6 @@
 defmodule Songbox.UserController do
   use Songbox.Web, :controller
 
-  alias Songbox.User
-  alias JaSerializer.Params
-
   plug :scrub_params, "data" when action in [:create, :update]
 
   def current(conn, _) do
