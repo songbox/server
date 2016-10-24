635df86f3d138c1097fabe2a29adf583d70e9ffb
Andreas Böhrnsen <andreas@frontrunner.io>
2016-10-24 16:58:11 +0200

    fix indentation and unused vars

--- web/controllers/auth_error_handler.ex
+++ web/controllers/auth_error_handler.ex
@@ -1,15 +1,15 @@
 defmodule Songbox.AuthErrorHandler do
- use Songbox.Web, :controller
+  use Songbox.Web, :controller
 
- def unauthenticated(conn, params) do
-  conn
-   |> put_status(401)
-   |> render(Songbox.ErrorView, "401.json")
- end
+  def unauthenticated(conn, _params) do
+    conn
+    |> put_status(401)
+    |> render(Songbox.ErrorView, "401.json")
+  end
 
- def unauthorized(conn, params) do
-  conn
-   |> put_status(403)
-   |> render(Songbox.ErrorView, "403.json")
- end
+  def unauthorized(conn, _params) do
+    conn
+    |> put_status(403)
+    |> render(Songbox.ErrorView, "403.json")
+  end
 end
