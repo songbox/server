ea90e03e370f79a74512d1a0f6e246fa1659fcc9
Andreas Böhrnsen <andreas@frontrunner.io>
2016-10-27 22:24:34 +0200

    fix unused vars

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,14 +6,14 @@ defmodule Songbox.UserSocket do
 
   transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000, check_origin: ["https://beta.songbox.co", "https://app.songbox.co"]
 
-  def connect(%{"token" => jwt} = params, socket) do
+  def connect(%{"token" => jwt} = _params, socket) do
     case sign_in(socket, jwt) do
-      {:ok, authed_socket, guardian_params} -> {:ok, authed_socket}
+      {:ok, authed_socket, _guardian_params} -> {:ok, authed_socket}
       _ -> :error
     end
   end
 
-  def connect(_params, socket) do
+  def connect(_params, _socket) do
     :error
   end
 
