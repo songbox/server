5bd4ee5a1a7bf6270e52741ad73e02d8b1ba2000
Andreas Böhrnsen <andreas@frontrunner.io>
2016-11-14 18:06:22 +0100

    add list_items assoc

--- web/views/list_view.ex
+++ web/views/list_view.ex
@@ -2,10 +2,26 @@ defmodule Songbox.ListView do
   use Songbox.Web, :view
   use JaSerializer.PhoenixView
 
-  attributes [:name, :inserted_at, :updated_at]
-  
-  has_one :user,
-    field: :user_id,
-    type: "user"
+  require Ecto.Query
 
+  alias Songbox.Repo
+
+  attributes [
+    :name,
+    :inserted_at,
+    :updated_at
+  ]
+
+  has_many :list_items, serializer: Songbox.ListItemView, include: true
+
+  def list_items(list, _conn) do
+    case list.list_items do
+      %Ecto.Association.NotLoaded{} ->
+        list
+        |> Ecto.assoc(:list_items)
+        |> Ecto.Query.order_by(asc: :rank)
+        |> Repo.all
+      other -> other
+    end
+  end
 end
